46ad9ad9ba718a7d613d20f8a0c93ef498fd04cb
Bexultan Myrzatay <bexultan.dev@gmail.com>
2019-09-24 22:43:50 +0600

    feat: create createApplication mutation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,8 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
 
-  # TODO: remove me
   field :allApplications, types[Types::Application] do
-    description "An example field added by the generator"
+    description "List all of the applications"
     resolve ->(obj, args, ctx) {
       Application.all
     }
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,3 @@
 class Application < ApplicationRecord
+    validates_presence_of :title, :description, :address, :latitude
 end
