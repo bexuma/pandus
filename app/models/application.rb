c01dca1f93c4382c96885a23512216af6355030f
Bexultan Myrzatay <bexultan.dev@gmail.com>
2019-09-25 11:29:08 +0600

    feat: generate latitude and longitude from address

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,6 @@
 class Application < ApplicationRecord
+    geocoded_by :address
+    after_validation :geocode, :if => :address_changed?
+    
     validates_presence_of :title, :description, :address
 end
